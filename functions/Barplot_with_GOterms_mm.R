d35da995ca801190866fc545e3f3139a985ad2e0
maria13 <mariakondili13@gmail.com>
2023-04-18 15:54:56 +0200

    add if-loop for EntrezID,sort  enrichGO-output

--- functions/Barplot_with_GOterms_mm.R
+++ functions/Barplot_with_GOterms_mm.R
@@ -3,22 +3,22 @@ create_barplot_GOterms.mm <- function(genenames,gene_format = "SYMBOL",my_ontol=
   suppressPackageStartupMessages(library(clusterProfiler))
   suppressPackageStartupMessages(library(org.Mm.eg.db))
   suppressPackageStartupMessages(library(AnnotationDbi))
-
-  EntrezID <- mapIds(x=org.Mm.eg.db, keys=genenames,
-                     column="ENTREZID",
-                     keytype=gene_format,
-                     multiVals = "first")
-  ##to see accepted keytypes :  keytypes(org.Mm.eg.db)
-  ## column = what I want to obtain
-  ## keytype = what I give in keys
-
   suppressPackageStartupMessages(library(DOSE))
   suppressPackageStartupMessages(library(enrichplot))
-  #library(clusterProfiler)
-  #library(pathview)
-  
-  
-  go.enrichm <- enrichGO(EntrezID, ont=my_ontol,
+
+  if (gene_format !="ENTREZID"){
+    EntrezID <- mapIds(x=org.Mm.eg.db, keys=genenames,
+                      column="ENTREZID",
+                      keytype=gene_format,
+                      multiVals = "first")
+      ##to see accepted keytypes :  keytypes(org.Mm.eg.db)
+      ## column = what I want to obtain
+      ## keytype = what I give in keys
+    } else {
+      EntrezID <- genenames
+    }
+
+    go.enrichm <- enrichGO(EntrezID, ont=my_ontol,
                             OrgDb=org.Mm.eg.db,
                             keyType = "ENTREZID",
                             pvalueCutoff=0.1,
@@ -26,9 +26,9 @@ create_barplot_GOterms.mm <- function(genenames,gene_format = "SYMBOL",my_ontol=
                             pAdjustMethod="BH",
                             readable=TRUE)
 
-  # go.enrichm.mf <- enrichGO(sig.data.d$Entrez_ID, ont="MF",OrgDb=org.Mm.eg.db,keyType = "ENTREZID",
-  #                           pvalueCutoff=0.05, qvalueCutoff=0.1, pAdjustMethod = "BH", readable=TRUE)
-  #
+    #sort GeneRatio from higher to lower :
+    go.enrichm@result$GeneRatio <- go.enrichm@result$GeneRatio[order(go@result$Count,decreasing = T)]
+
 
   if (my_ontol=="BP") {msg = "Biol_Processes" }
   if (my_ontol=="MF") {msg = "Molec_Functions"}
@@ -36,17 +36,17 @@ create_barplot_GOterms.mm <- function(genenames,gene_format = "SYMBOL",my_ontol=
   if (my_ontol== "ALL") {msg="All_GO_terms"}
 
   dir.create("GO_terms_Plots/",showWarnings = F)
-  if ( in_png==TRUE) { 
-    png(paste0("GO_terms_Plots/Barplot_",msg,"_",main,".png"))
-      p <- barplot(go.enrichm, showCategory = 15,
-                  x="GeneRatio", color="p.adjust", font.size=8,
-                  title=paste(msg," of ", main))
+
+  p <- barplot(go.enrichm, showCategory = 15,
+              x="GeneRatio", color="p.adjust", font.size=8,
+              title=paste(msg," of ", main))
+
+  if ( in_png==TRUE) {
+      png(paste0("GO_terms_Plots/Barplot_",msg,"_",main,".png"))
+          print(p)
+      dev.off()
+  } else {
       print(p)
-    dev.off()
-  } else{ 
-    barplot(go.enrichm, showCategory = 15,
-            x="GeneRatio", color="p.adjust", font.size=8,
-            title=paste(msg," of ", main))
     }
-  
+
 }
